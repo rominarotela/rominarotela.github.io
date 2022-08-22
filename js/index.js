ce159200dbfebd289e042757cebdcd9988b124dc
rominarotela <romy_rotela@fpuna.edu.py>
2022-08-22 12:42:41 -0400

    js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/* $(document).ready(function(){
+    
+    $("#acerca").click(function () {
+        $('html, body').animate({
+            scrollTop: $('#el_seminario').offset().top
+        }, 'slow');
+    });
+}); */
+
+console.log("entro aqui")
+
